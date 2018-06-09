bc6ccf0a526171126bb97049ddf079906a9d4743
qq452057496 <qq452057496@outlook.com>
2018-06-09 16:34:38 +0800

    111

---
+++ src/zy1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zy1
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("哈哈");
+        }
+    }
+}
